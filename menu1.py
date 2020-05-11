247c6f3a4d4f8a5e72971f61d92f3839981d740b
pjkfckr <61726800+pjkfckr@users.noreply.github.com>
2020-05-11 14:47:33 +0900

    Update menu1.py

--- menu1.py
+++ menu1.py
@@ -23,7 +23,7 @@ while True:  ## 위에서 음식을 추가 하는 loop를 종료 한 뒤에는 �
     else:
         set_lunch = set_lunch - set([item]) ## 위에 set_lunch가 실질적으로 이루어지는 곳이다. 입력한item의 값을 집합으로 만들기 위해 set([item])을 사용하였다. []을 사용하여 item을 리스트로 만든뒤 set()으로 집합을 만들어준다. 위에 set_lunch는 set(lunch)의 값을 저장하였고, 이 set_lunch의 값에 set으로 만들어준 set([item])을 차집합 한 값이 저장이 된다.
 
-print(random.choice(set_lunch), "중에서 선택합니다.")
+print(set_lunch, "중에서 선택합니다.")
 print("5")   
 time.sleep(1)    ## 위에서 import time을 한 이유는 time.sleep(sec)을 쓰기 위해서 이다. 이 기능은 ()사이에 주어진 초 만큼 쉰 다음 그 다음의 값이 출력되게 하는 기능이다.
 print("4")
@@ -34,4 +34,4 @@ print("2")
 time.sleep(1)
 print("1")
 time.sleep(1)
-print(random.choice(list(set_lunch))) ## import random 을 사용한 이유가 여기 있다. 먼저 주어진 값이 set()의 형태로 저장된 set_lunch를 random.choice를 사용하여 그중 값을 하나 랜덤하게 출력한다. 하지만 random.choice는 list의 형태에서만 동작을 하기때문에 set_lunch를 list()로 묶어준뒤 출력하게 작성하였다.
+print((random.choice(list(set_lunch)))) ## import random 을 사용한 이유가 여기 있다. 먼저 주어진 값이 set()의 형태로 저장된 set_lunch를 random.choice를 사용하여 그중 값을 하나 랜덤하게 출력한다. 하지만 random.choice는 list의 형태에서만 동작을 하기때문에 set_lunch를 list()로 묶어준뒤 출력하게 작성하였다.
